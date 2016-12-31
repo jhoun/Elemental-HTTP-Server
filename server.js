37b3a2384a7abb30eb4b7ffe1607ad1baa178ad6
jhoun <jayhoun@gmail.com>
2016-12-31 10:17:31 -1000

    able to load html and css with readir

--- server.js
+++ server.js
@@ -3,71 +3,106 @@ const fs = require('fs');
 const querystring = require('querystring');
 
 let clientRequestPath;
+let haveFile = false;
 
 const server = http.createServer((req, res) => {
+  //removes '/' from url
 
-  if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
-    clientRequestPath =  './public/index.html';
-    fs.readFile(clientRequestPath, (err, file) => {
-    if (err) throw err;
-    res.write(file);
-    res.end();
-  });
-  } else if (req.method === 'GET' && req.url === '/helium.html'){
-    clientRequestPath =  './public/helium.html';
-    fs.readFile(clientRequestPath, (err, file) => {
-    if (err) throw err;
-    res.write(file);
-    res.end();
-  });
-  } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
-    clientRequestPath =  './public/hydrogen.html';
-    fs.readFile(clientRequestPath, (err, file) => {
-    if (err) throw err;
-    res.write(file);
-    res.end();
-  });
-  } else if (req.method === 'GET' && req.url === '/css/styles.css') {
-    clientRequestPath =  './public/css/styles.css';
-     fs.readFile(clientRequestPath, (err, file) => {
-    if (err) throw err;
-    res.write(file);
-    res.end();
-  });
-  } else if  (req.method === 'POST'){
+  var url = req.url.substring(1);
 
-    //client POST request
-    req.on('data', (data) => {
-      var clientHeader = querystring.parse(data.toString());
-      console.log(clientHeader.elementName);
+            console.log(url);
 
-      //writes a new file
-      fs.writeFile(`./public/${clientHeader.elementName}.html`, `<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>The Elements - Helium</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <h1>${clientHeader.elementName}</h1>
-  <h2>${clientHeader.elementSymbol}</h2>
-  <h3>Atomic number ${clientHeader.elementAtomicNumber}</h3>
-  <p>${clientHeader.elementDescription}</p>
-  <p><a href="/">back</a></p>
-</body>
-</html>`, (err) => {
+  //Pushes all files in public directory into array
+    fs.readdir('./public', (err, filesInDir) => {
+      if (err) throw err;
+  // checks all files in public directory
+    filesInDir.forEach(function(file){
+      if (req.method === "GET" && url === file) {
+        fs.readFile(`./public/${file}`, (err, file) => {
         if (err) throw err;
-        clientRequestPath =  './public/boron.html';
-        console.log('It\'s saved!');
-
-          fs.readFile(clientRequestPath, (err, file) => {
-            if (err) throw err;
-            res.write(file);
-            res.end();
-          });
-      });
+        res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': `${file.length}`});
+        res.write(file);
+        res.end();
+        });
+       } else if (url === 'css/styles.css'){
+        fs.readFile('./public/css/styles.css', (err, file) => {
+        if (err) throw err;
+        res.writeHead(200, {
+        'Content-Type': 'text/css',
+        'Content-Length': `${file.length}`});
+        res.write(file);
+        });
+      }
     });
-  }
+  });
+
+
+
+//   if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
+//     clientRequestPath =  './public/index.html';
+//     fs.readFile(clientRequestPath, (err, file) => {
+//     if (err) throw err;
+//     res.write(file);
+//     res.end();
+//   });
+//   } else if (req.method === 'GET' && req.url === '/helium.html'){
+//     clientRequestPath =  './public/helium.html';
+//     fs.readFile(clientRequestPath, (err, file) => {
+//     if (err) throw err;
+//     res.write(file);
+//     res.end();
+//   });
+//   } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
+//     clientRequestPath =  './public/hydrogen.html';
+//     fs.readFile(clientRequestPath, (err, file) => {
+//     if (err) throw err;
+//     res.write(file);
+//     res.end();
+//   });
+//   } else if (req.method === 'GET' && req.url === '/css/styles.css') {
+//     clientRequestPath =  './public/css/styles.css';
+//      fs.readFile(clientRequestPath, (err, file) => {
+//     if (err) throw err;
+//     res.write(file);
+//     res.end();
+//   });
+//   } else if  (req.method === 'POST'){
+
+//     //client POST request
+//     req.on('data', (data) => {
+//       var clientHeader = querystring.parse(data.toString());
+//       console.log(clientHeader.elementName);
+
+//       //writes a new file
+//       fs.writeFile(`./public/${clientHeader.elementName}.html`, `<html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <title>The Elements - ${clientHeader.elementName}</title>
+//   <link rel="stylesheet" href="/css/styles.css">
+// </head>
+// <body>
+//   <h1>${clientHeader.elementName}</h1>
+//   <h2>${clientHeader.elementSymbol}</h2>
+//   <h3>Atomic number ${clientHeader.elementAtomicNumber}</h3>
+//   <p>${clientHeader.elementDescription}</p>
+//   <p><a href="/">back</a></p>
+// </body>
+// </html>`, (err) => {
+//         if (err) throw err;
+//         clientRequestPath =  './public/${clientHeader.elementName}.html';
+//         console.log('It\'s saved!');
+
+//           fs.readFile(clientRequestPath, (err, file) => {
+//             if (err) throw err;
+//             res.write(file);
+//             res.end();
+//           });
+//       });
+//     });
+  // }
+
 
 });
 
