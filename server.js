1d664ac69812cf9333b93d1dbd30e908d264326a
jhoun <jayhoun@gmail.com>
2016-12-30 14:15:03 -1000

    static files properly sent to client

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 
+let clientRequestPath;
+
 const server = http.createServer((req, res) => {
 
-  fs.readFile('./public/helium.html', (err, heliumFile) => {
+  if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
+    clientRequestPath =  './public/index.html';
+  } else if (req.method === 'GET' && req.url === '/helium.html'){
+    clientRequestPath =  './public/helium.html';
+  } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
+    clientRequestPath =  './public/hydrogen.html';
+  } else if (req.method === 'GET' && req.url === '/css/styles.css') {
+    clientRequestPath =  './public/css/styles.css';
+  }
+
+
+  fs.readFile(clientRequestPath, (err, file) => {
     if (err) throw err;
-    res.write(heliumFile);
+    res.write(file);
     res.end();
   });
 });
