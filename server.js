52f009671c45afc79eda97e3c61cd77fd17bf592
jhoun <jayhoun@gmail.com>
2016-12-30 15:51:57 -1000

    got post to work, but trying to get it to work with get

--- server.js
+++ server.js
@@ -6,30 +6,60 @@ let clientRequestPath;
 
 const server = http.createServer((req, res) => {
 
-  // if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
-  //   clientRequestPath =  './public/index.html';
-  // } else if (req.method === 'GET' && req.url === '/helium.html'){
-  //   clientRequestPath =  './public/helium.html';
-  // } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
-  //   clientRequestPath =  './public/hydrogen.html';
-  // } else if (req.method === 'GET' && req.url === '/css/styles.css') {
-  //   clientRequestPath =  './public/css/styles.css';
-  // }
-
-
-  if (req.method === 'POST'){
-    req.on('data', (data) => {
-      console.log(querystring.parse(data.toString()));
-    })
+  if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
+    clientRequestPath =  './public/index.html';
+  } else if (req.method === 'GET' && req.url === '/helium.html'){
+    clientRequestPath =  './public/helium.html';
+  } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
+    clientRequestPath =  './public/hydrogen.html';
+  } else if (req.method === 'GET' && req.url === '/css/styles.css') {
+    clientRequestPath =  './public/css/styles.css';
   }
 
 
+  if (req.method === 'POST'){
 
-  // fs.readFile(clientRequestPath, (err, file) => {
-  //   if (err) throw err;
-  //   res.write(file);
-  //   res.end();
-  // });
+    //client POST request
+    req.on('data', (data) => {
+      var clientHeader = querystring.parse(data.toString());
+      console.log(clientHeader.elementName);
+      console.log("hi");
+
+      //writes a new file
+      fs.writeFile(`./public/${clientHeader.elementName}.html`, `<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - Helium</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>${clientHeader.elementName}</h1>
+  <h2>${clientHeader.elementSymbol}</h2>
+  <h3>Atomic number ${clientHeader.elementAtomicNumber}</h3>
+  <p>${clientHeader.elementDescription}</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`, (err) => {
+        if (err) throw err;
+        clientRequestPath =  './public/boron.html';
+        console.log('It\'s saved!');
+
+          fs.readFile(clientRequestPath, (err, file) => {
+            if (err) throw err;
+            res.write(file);
+            res.end();
+          });
+      });
+    });
+  }
+
+
+  //reads the file
+  fs.readFile(clientRequestPath, (err, file) => {
+    if (err) throw err;
+    res.write(file);
+    res.end();
+  });
 
 });
 
