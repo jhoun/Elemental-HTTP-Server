7bd28f2311ddc5c0a6cbf5611388646e84b5e29b
jhoun <jayhoun@gmail.com>
2017-01-02 10:20:25 -1000

    able to check to see if file is already in my directory, if not then the post file is created

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ let fileType;
 
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   //removes '/' from url
   var findCss = req.url.split('').splice(-3, 3).join('');
 
@@ -15,14 +16,60 @@ const server = http.createServer((req, res) => {
     fileType = 'html';
   }
 
+// checks to see if we have the file in our directory
+fs.readFile(`./public${req.url}`, (err) => {
+  if (req.url === "/"){
+    fs.readFile('./public/index.html', (err, file) => {
+      if (err) throw err;
+      res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': `${file.length}`});
+      res.write(file);
+      res.end()
+    });
+  } else if (err) {
+    console.log('this file does not exists, so we are gonna make one');
 
+    // client POST request
+    req.on('data', (data) => {
+    var clientHeader = querystring.parse(data.toString());
+    console.log('client wants: ',clientHeader.elementName);
+
+    //creates the requested file and html input
+    fs.writeFile(`./public/${clientHeader.elementName}.html`, `<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - ${clientHeader.elementName}</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>${clientHeader.elementName}</h1>
+  <h2>${clientHeader.elementSymbol}</h2>
+  <h3>Atomic number ${clientHeader.elementAtomicNumber}</h3>
+  <p>${clientHeader.elementDescription}</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`, (err) => {
+        if (err) throw err;
+        //creates new path
+        clientRequestPath =  `./public/${clientHeader.elementName}.html`;
+        console.log('It\'s saved!');
+        //reads it back out to client
+          fs.readFile(clientRequestPath, (err, file) => {
+            if (err) throw err;
+            res.write(file);
+            res.end();
+          });
+      });
+    });
+  } else {
   //checks to see if we have this file
   fs.readFile(`./public${req.url}`, (err, file) => {
     //if we don't, then throw custom error
   if (req.url === "/"){
     fs.readFile('./public/index.html', (err, file) => {
       if (err) throw err;
-      res.writeHead(404, {
+      res.writeHead(200, {
       'Content-Type': 'text/html',
       'Content-Length': `${file.length}`});
       res.write(file);
@@ -50,120 +97,12 @@ const server = http.createServer((req, res) => {
     }
   });
 
-
-//   // Pushes all files in public directory into array
-//     fs.readdir('./public', (err, filesInDir) => {
-//       if (err) throw err;
-
-//   // checks all files in public directory
-//     for (var i = 0; i < filesInDir.length; i++){
-
-//       if (url === filesInDir[i]) {
-//         fs.readFile(` ./public/${filesInDir[i]}`, (err, file) => {
-//         fileFound = true;
-//         if (err) throw err;
-//         res.writeHead(200, {
-//         'Content-Type': 'text/html',
-//         'Content-Length': `${file.length}`});
-//         res.end(file);
-//         });
-//       } else if (req.url === '/'){
-//         fileFound = true;
-//         fs.readFile(`./public/index.html`, (err, file) => {
-//         if (err) throw err;
-//         res.writeHead(200, {
-//         'Content-Type': 'text/html',
-//         'Content-Length': `${file.length}`});
-//         res.write(file);
-//         });
-//       } else if (url === 'css/styles.css'){
-//         fileFound = true;
-//         fs.readFile('./public/css/styles.css', (err, file) => {
-//         if (err) throw err;
-//         res.writeHead(200, {
-//         'Content-Type': 'text/css',
-//         'Content-Length': `${file.length}`});
-//         res.write(file);
-//         });
-//       }
-//     };
-//       if (fileFound === false){
-//         fs.readFile('./public/404.html', (err, file) => {
-//         if (err) throw err;
-//         res.writeHead(404, {
-//         'Content-Type': 'text/html',
-//         'Content-Length': `${file.length}`});
-//         res.write(file);
-//         });
-//       }
-//   });
+  }
+});
 
 });
 
 server.listen(8080,() => {
   console.log('opened server on', server.address());
 });
-
-//   if (req.method === 'GET' && req.url === '/' || req.url === '/index.html'){
-//     clientRequestPath =  './public/index.html';
-//     fs.readFile(clientRequestPath, (err, file) => {
-//     if (err) throw err;
-//     res.write(file);
-//     res.end();
-//   });
-//   } else if (req.method === 'GET' && req.url === '/helium.html'){
-//     clientRequestPath =  './public/helium.html';
-//     fs.readFile(clientRequestPath, (err, file) => {
-//     if (err) throw err;
-//     res.write(file);
-//     res.end();
-//   });
-//   } else if (req.method === 'GET' && req.url === '/hydrogen.html') {
-//     clientRequestPath =  './public/hydrogen.html';
-//     fs.readFile(clientRequestPath, (err, file) => {
-//     if (err) throw err;
-//     res.write(file);
-//     res.end();
-//   });
-//   } else if (req.method === 'GET' && req.url === '/css/styles.css') {
-//     clientRequestPath =  './public/css/styles.css';
-//      fs.readFile(clientRequestPath, (err, file) => {
-//     if (err) throw err;
-//     res.write(file);
-//     res.end();
-//   });
-//   } else if  (req.method === 'POST'){
-
-//     //client POST request
-//     req.on('data', (data) => {
-//       var clientHeader = querystring.parse(data.toString());
-//       console.log(clientHeader.elementName);
-
-//       //writes a new file
-//       fs.writeFile(`./public/${clientHeader.elementName}.html`, `<html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <title>The Elements - ${clientHeader.elementName}</title>
-//   <link rel="stylesheet" href="/css/styles.css">
-// </head>
-// <body>
-//   <h1>${clientHeader.elementName}</h1>
-//   <h2>${clientHeader.elementSymbol}</h2>
-//   <h3>Atomic number ${clientHeader.elementAtomicNumber}</h3>
-//   <p>${clientHeader.elementDescription}</p>
-//   <p><a href="/">back</a></p>
-// </body>
-// </html>`, (err) => {
-//         if (err) throw err;
-//         clientRequestPath =  './public/${clientHeader.elementName}.html';
-//         console.log('It\'s saved!');
-
-//           fs.readFile(clientRequestPath, (err, file) => {
-//             if (err) throw err;
-//             res.write(file);
-//             res.end();
-//           });
-//       });
-//     });
-  // }
 
